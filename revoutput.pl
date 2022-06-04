e2d39ebc3e76a34dc581eaaa6bf5c37aee23a77f
GorillaSapiens <embracetheape@gmail.com>
2022-06-04 11:29:29 -0400

    reverse compare and output

---
+++ revoutput.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S perl -w
+
+print "<html>\n";
+print "<body>\n";
+print "<table>\n";
+
+while (<>) {
+   s/[\x0a\x0d]//g;
+   ($score,$output,$reference) = split / /;
+   print "<tr>\n";
+   print "   <td>$score</td>\n";
+   print "   <td><img src=references/$output><br>$output</td>\n";
+   print "   <td><img src=output/$reference><br>$reference</td>\n";
+   print "</tr>\n";
+}
+
+print "</table>\n";
+print "</body>\n";
+print "</html>\n";
